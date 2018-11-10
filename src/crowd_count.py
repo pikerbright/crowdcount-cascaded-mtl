54defcb667777b11b7c8d18961ee272b6b00b9a3
zhuliang <zhuliang@qiniu.com>
2018-11-10 21:30:28 +0800

    u

--- src/crowd_count.py
+++ src/crowd_count.py
@@ -68,17 +68,17 @@ class CrowdCounter(nn.Module):
         return self.loss_mse
     
     def forward(self,  im_data, gt_data=None, gt_cls_label=None, ce_weights=None):        
-        im_data = network.np_to_variable(im_data, is_cuda=False, is_training=self.training)
+        im_data = network.np_to_variable(im_data, is_cuda=True, is_training=self.training)
         density_map = self.CCN(im_data)
 
         if self.training:                        
-            gt_data = network.np_to_variable(gt_data, is_cuda=False, is_training=self.training)
-            self.loss_mse = self.build_loss(density_map, gt_data, ce_weights)
+            gt_data = network.np_to_variable(gt_data, is_cuda=True, is_training=self.training)
+            self.loss_mse = self.build_loss(density_map, gt_data)
             
             
         return density_map
     
-    def build_loss(self, density_map, gt_data, ce_weights):
+    def build_loss(self, density_map, gt_data):
         loss_mse = self.loss_mse_fn(density_map, gt_data)        
 
         return loss_mse
